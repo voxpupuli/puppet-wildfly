4f5745491287674628fed63623cbcfdfef2a0cec
Edwin Biemond <biemond@gmail.com>
2014-06-22 01:34:07 +0200

    refactored

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,12 +5,12 @@ class wildfly::install(
   $install_source  = undef,
   $install_file    = undef,
   $java_home       = undef,
-  $group           = $wildfly::params::wildfly_group,
-  $user            = $wildfly::params::wildfly_user,
-  $dirname         = $wildfly::params::wildfly_dirname,
-  $service_file    = $wildfly::params::wildfly_service_file,
-  $mode            = $wildfly::params::wildfly_mode,
-  $config          = $wildfly::params::wildfly_config,
+  $group           = $wildfly::params::group,
+  $user            = $wildfly::params::user,
+  $dirname         = $wildfly::params::dirname,
+  $service_file    = $wildfly::params::service_file,
+  $mode            = $wildfly::params::mode,
+  $config          = $wildfly::params::config,
 ) inherits wildfly::params  {
 
   group { $group :
@@ -76,7 +76,6 @@ class wildfly::install(
     content    => template("wildfly/${service_file}"),
   }
 
-
   file{'/etc/default/wildfly.conf':
     ensure     => present,
     mode       => '0755',
@@ -85,5 +84,13 @@ class wildfly::install(
     content    => template("wildfly/wildfly.conf.erb"),
   }
 
+  service { "wildfly":
+    name       => "wildfly",
+    enable     => true,
+    ensure     => true,
+    require    => [File['/etc/init.d/wildfly'],
+                   File['/etc/default/wildfly.conf'],
+                   Exec["tar ${install_file} in /var/tmp"],]
+  }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,21 +2,39 @@
 #
 class wildfly::params {
 
-  $wildfly_group   = 'wildfly'
-  $wildfly_user    = 'wildfly'
-  $wildfly_dirname = '/opt/wildfly'
+  $group   = 'wildfly'
+  $user    = 'wildfly'
+  $dirname = '/opt/wildfly'
 
-  $wildfly_service_file  = $::osfamily? {
-    Debian => 'wildfly-init-debian.sh.erb',
-    RedHat => 'wildfly-init-redhat.sh.erb',
+  $service_file  = $::osfamily? {
+    Debian  => 'wildfly-init-debian.sh.erb',
+    RedHat  => 'wildfly-init-redhat.sh.erb',
     default => 'wildfly-init-redhat.sh.erb',
   } 
 
-  # => Wildfly Deployment Type (standalone or domain)
-  $wildfly_mode = 'standalone'
+  $mode              = 'standalone'
+  $config            = 'standalone-full.xml'
 
-  # => Standalone Mode Configuration
-  # => (standalone/ha.xml, standalone-full/ha/ha-aws.xml)
-  $wildfly_config = 'standalone-full.xml'
+  $mgmt_bind         = '0.0.0.0'
+  $mgmt_http_port    = '9990'
+  $mgmt_https_port   = '9993'
+
+  $public_bind       = '0.0.0.0'
+  $public_http_port  = '8080'
+  $public_https_port = '8443'
+
+  $wsdl_bind         = '0.0.0.0'
+  $ajp_port          = '8009'
+
+  $java_xmx          = '512m'
+  $java_xms          = '64m'
+  $java_maxpermsize  = '256m'
+
+  $users_mgmt = { 
+    'wildfly' => { 
+      username => 'wildfly', 
+      password => '2c6368f4996288fcc621c5355d3e39b7',
+    },
+  }
 
 }
