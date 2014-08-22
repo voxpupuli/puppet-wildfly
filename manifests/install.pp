8cadf575358cec6ddc1120521c9fcc4cac7fd808
Edwin Biemond <edwin@edwins-mbp.lan>
2014-08-22 11:04:24 +0200

    formatting

--- manifests/install.pp
+++ manifests/install.pp
@@ -49,10 +49,10 @@ class wildfly::install(
     require    => User[$user],
   }
 
-  $libaiopackage  = $::osfamily ? { 
-    'RedHat' => "libaio",
-    'Debian' => "libaio1",
-    default  => "libaio",
+  $libaiopackage  = $::osfamily ? {
+    'RedHat' => 'libaio',
+    'Debian' => 'libaio1',
+    default  => 'libaio',
   }
 
   if !defined(Package[$libaiopackage]) {
@@ -88,76 +88,76 @@ class wildfly::install(
 
   # file /opt/wildfly/bin/standalone.conf
   # default JAVA_OPTS="-Xms64m -Xmx512m -XX:MaxPermSize=256m
-  exec { "replace memory parameters":
+  exec { 'replace memory parameters':
     command => "sed -i -e's/\\-Xms64m \\-Xmx512m \\-XX:MaxPermSize=256m/\\-Xms${java_xms} \\-Xmx${java_xmx} \\-XX:MaxPermSize=${java_maxpermsize}/g' ${dirname}/bin/standalone.conf",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep '\\-Xms64m \\-Xmx512m \\-XX:MaxPermSize=256m' ${dirname}/bin/standalone.conf",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  Exec["replace management bind"] ->
-    Exec["replace public bind"] ->
-      Exec["replace management http port"] ->
-        Exec["replace management https port"] ->
-          Exec["replace http port"] ->
-            Exec["replace https port"] ->
-              Exec["replace ajp port"]
+  Exec['replace management bind'] ->
+    Exec['replace public bind'] ->
+      Exec['replace management http port'] ->
+        Exec['replace management https port'] ->
+          Exec['replace http port'] ->
+            Exec['replace https port'] ->
+              Exec['replace ajp port']
 
-  exec { "replace management bind":
+  exec { 'replace management bind':
     command => "sed -i -e's/jboss.bind.address.management:127.0.0.1/jboss.bind.address.management:${mgmt_bind}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.bind.address.management:127.0.0.1' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace public bind":
+  exec { 'replace public bind':
     command => "sed -i -e's/jboss.bind.address:127.0.0.1/jboss.bind.address:${public_bind}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.bind.address:127.0.0.1' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace management http port":
+  exec { 'replace management http port':
     command => "sed -i -e's/jboss.management.http.port:9990/jboss.management.http.port:${mgmt_http_port}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.management.http.port:9990' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace management https port":
+  exec { 'replace management https port':
     command => "sed -i -e's/jboss.management.https.port:9993/jboss.management.https.port:${mgmt_https_port}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.management.https.port:9993' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace http port":
+  exec { 'replace http port':
     command => "sed -i -e's/jboss.http.port:8080/jboss.http.port:${public_http_port}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.http.port:8080' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace https port":
+  exec { 'replace https port':
     command => "sed -i -e's/jboss.https.port:8443/jboss.https.port:${public_https_port}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.https.port:8443' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
-  exec { "replace ajp port":
+  exec { 'replace ajp port':
     command => "sed -i -e's/jboss.ajp.port:8009/jboss.ajp.port:${ajp_port}/g' ${dirname}/standalone/configuration/${config}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     onlyif  => "grep 'jboss.ajp.port:8009' ${dirname}/standalone/configuration/${config}",
     require => Exec["tar ${install_file} in /var/tmp"],
-    before  => Service["wildfly"],
+    before  => Service['wildfly'],
   }
 
   file{"${dirname}/standalone/configuration/mgmt-users.properties":
@@ -165,8 +165,8 @@ class wildfly::install(
     mode       => '0755',
     owner      => $user,
     group      => $group,
-    content    => template("wildfly/mgmt-users.properties.erb"),
-    before     => Service["wildfly"],
+    content    => template('wildfly/mgmt-users.properties.erb'),
+    before     => Service['wildfly'],
   }
 
   file{'/etc/init.d/wildfly':
@@ -182,12 +182,12 @@ class wildfly::install(
     mode       => '0755',
     owner      => 'root',
     group      => 'root',
-    content    => template("wildfly/wildfly.conf.erb"),
+    content    => template('wildfly/wildfly.conf.erb'),
   }
 
-  service { "wildfly":
+  service { 'wildfly':
     ensure     => true,
-    name       => "wildfly",
+    name       => 'wildfly',
     enable     => true,
     require    => [File['/etc/init.d/wildfly'],File['/etc/default/wildfly.conf'],Exec["tar ${install_file} in /var/tmp"],]
   }
