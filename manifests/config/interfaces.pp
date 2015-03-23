b3f498d5089aa04183cd134048e565f5dbf7afe2
biemond <biemond@gmail.com>
2015-03-23 12:21:13 +0100

    augeas fix

--- manifests/config/interfaces.pp
+++ manifests/config/interfaces.pp
@@ -7,7 +7,7 @@ define wildfly::config::interfaces(
 {
   augeas { $title:
     lens    => 'Xml.lns',
-    incl    => $wildfly::config_file_path,
+    incl    => "${wildfly::dirname}/${wildfly::mode}/configuration/${wildfly::config}",
     changes => "set server/interfaces/interface[#attribute/name='${title}']/inet-address/#attribute/value ${inet_address_value}",
     onlyif  => "match server/interfaces/interface[#attribute/name='${title}']/inet-address[#attribute/value='${inet_address_value}'] size == 0"
   }
--- manifests/config/socket_binding.pp
+++ manifests/config/socket_binding.pp
@@ -7,7 +7,7 @@ define wildfly::config::socket_binding(
 
   augeas { $title:
     lens    => 'Xml.lns',
-    incl    => $wildfly::config_file_path,
+    incl    => "${wildfly::dirname}/${wildfly::mode}/configuration/${wildfly::config}",
     changes => "set server/socket-binding-group/socket-binding[#attribute/name='${title}']/#attribute/port ${port}",
     onlyif  => "match server/socket-binding-group/socket-binding[#attribute/name='${title}'][#attribute/port='${port}'] size == 0"
   }
