d99765da5878820d5f89ddefe3a4282c266ba0eb
Jairo Junior <jairo.sjunior@dataprev.gov.br>
2015-04-11 12:46:32 -0300

    Small exec_cli tunning.

--- manifests/util/exec_cli.pp
+++ manifests/util/exec_cli.pp
@@ -4,7 +4,7 @@
 define wildfly::util::exec_cli($condition = 'success', $post_exec_command = 'exit', $action_command = undef, $verify_command = undef) {
 
   exec { "JBoss-CLI: ${title}":
-    command => "jboss-cli.sh -c --command='${action_command}' && jboss-cli.sh -c --command='${post_exec_command}'",
+    command => "jboss-cli.sh -c --commands='${action_command},${post_exec_command}'",
     unless  => "jboss-cli.sh -c --command='${verify_command}' | grep '${condition}'",
     path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin', "${wildfly::dirname}/bin"],
     require => Service['wildfly']
