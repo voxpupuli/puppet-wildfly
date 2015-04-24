6c2d69e3f7f60e7b82df7ff767879320bd13d4a3
Jairo Junior <jairo.sjunior@dataprev.gov.br>
2015-04-24 20:01:05 -0300

    puppet-lint --fix.

--- manifests/standalone/undertow/https.pp
+++ manifests/standalone/undertow/https.pp
@@ -3,16 +3,16 @@
 #
 define wildfly::standalone::undertow::https($socket_binding = undef, $keystore_path = undef, $keystore_password = undef, $key_alias = undef, $key_password = undef) {
 
-  wildfly::util::cli { "/core-service=management/security-realm=TLSRealm":
+  wildfly::util::cli { '/core-service=management/security-realm=TLSRealm':
     content => { }
   }
   ->
-  wildfly::util::cli { "/core-service=management/security-realm=TLSRealm/server-identity=ssl":
+  wildfly::util::cli { '/core-service=management/security-realm=TLSRealm/server-identity=ssl':
     content => {
-      'keystore-path' => $keystore_path,
+      'keystore-path'     => $keystore_path,
       'keystore-password' => $keystore_password,
-      'alias' => $key_alias,
-      'key-password' => $key_password
+      'alias'             => $key_alias,
+      'key-password'      => $key_password
     }
   }
   ->
