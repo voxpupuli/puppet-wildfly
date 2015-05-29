2b0f1ccfb3254e52237ba5d848c0efcb6371ee08
Mark McGuire <mmcguire@backstopsolutions.com>
2015-05-29 09:48:43 -0500

    add xa_datasource

---
+++ manifests/standalone/datasources/xa_datasource.pp
@@ -0,0 +1,15 @@
+#
+# Configures a datasource
+#
+define wildfly::standalone::datasources::xa_datasource($config = undef) {
+
+  wildfly::util::resource { "/subsystem=datasources/xa-data-source=${name}":
+    content => $config,
+  }
+  ->
+  wildfly::util::exec_cli { "Enable ${name}":
+    command => "/subsystem=datasources/xa-data-source=${name}:enable",
+    unless  => "(result == true) of /subsystem=datasources/xa-data-source=${name}:read-attribute(name=enabled)"
+  }
+
+}
