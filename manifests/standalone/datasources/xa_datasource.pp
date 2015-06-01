891f240dfdcbf339191d0b4741247d6a0373d6fc
Mark McGuire <mark.b.mcg@gmail.com>
2015-06-01 14:49:24 -0500

    make xa_datasource use recursive => true

--- manifests/standalone/datasources/xa_datasource.pp
+++ manifests/standalone/datasources/xa_datasource.pp
@@ -5,6 +5,7 @@ define wildfly::standalone::datasources::xa_datasource($config = undef) {
 
   wildfly::util::resource { "/subsystem=datasources/xa-data-source=${name}":
     content => $config,
+    recursive => true
   }
   ->
   wildfly::util::exec_cli { "Enable ${name}":
