5c19aa9a0baaa25a62bc6d350a890db12e251310
tstibbs <tstibbs@users.noreply.github.com>
2015-06-09 08:44:31 +0100

    Minimum changes required to deploy jboss EAP instead of wildfly

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,24 @@
 #
 # Wildfly default params class
 #
-class wildfly::params {
+class wildfly::params (
+  $custom_service_file = undef # can be set by hiera to override the default service file location
+) {
 
   $manage_user = true
   $group       = 'wildfly'
   $user        = 'wildfly'
   $dirname     = '/opt/wildfly'
 
-  $service_file  = $::osfamily? {
-    'Debian' => 'wildfly-init-debian.sh',
-    'RedHat' => 'wildfly-init-redhat.sh',
-    default  => 'wildfly-init-redhat.sh',
+  if $custom_service_file != undef {
+	$service_file = $custom_service_file
+  }
+  else {
+    $service_file  = $::osfamily? {
+      'Debian' => 'wildfly-init-debian.sh',
+      'RedHat' => 'wildfly-init-redhat.sh',
+      default  => 'wildfly-init-redhat.sh',
+    }
   }
 
   $java_home         = '/usr/java/jdk1.7.0_75/'
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,10 @@
 #
 # Wildfly startup service class
 #
-class wildfly::service {
+class wildfly::service (
+	$custom_wildfly_conf_file = undef, # can be set by hiera to override the default conf file location
+	$service_name = "wildfly"
+){
 
   $java_home = $wildfly::java_home
   $dirname = $wildfly::dirname
@@ -10,15 +13,20 @@ class wildfly::service {
   $config= $wildfly::config
   $console_log=$wildfly::console_log
 
-  case $::osfamily {
-    'RedHat': {
-      $wildfly_conf_file = '/etc/default/wildfly.conf'
-    }
-    'Debian': {
-      $wildfly_conf_file = '/etc/default/wildfly'
-    }
-    default: {
-      $wildfly_conf_file = '/etc/default/wildfly.conf'
+  if $custom_wildfly_conf_file != undef {
+	$wildfly_conf_file = $custom_wildfly_conf_file
+  }
+  else {
+    case $::osfamily {
+      'RedHat': {
+        $wildfly_conf_file = "/etc/default/${service_name}.conf"
+      }
+      'Debian': {
+        $wildfly_conf_file = "/etc/default/${service_name}"
+      }
+      default: {
+        $wildfly_conf_file = "/etc/default/${service_name}.conf"
+      }
     }
   }
 
@@ -30,7 +38,7 @@ class wildfly::service {
     content => template('wildfly/wildfly.conf.erb'),
   }
 
-  file { '/etc/init.d/wildfly':
+  file { "/etc/init.d/${service_name}":
     ensure => present,
     mode   => '0755',
     owner  => 'root',
@@ -38,12 +46,12 @@ class wildfly::service {
     source => "${wildfly::dirname}/bin/init.d/${wildfly::service_file}",
   }
 
-  service { 'wildfly':
+  service { $service_name:
     ensure     => true,
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    require    => [File['/etc/init.d/wildfly'],File[$wildfly_conf_file]]
+    require    => [File["/etc/init.d/${service_name}"],File[$wildfly_conf_file]]
   }
 
 }
