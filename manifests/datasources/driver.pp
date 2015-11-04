2b436674203ebbfbcf5be22c3cd1ecb5796bceac
Bastiaan Schaap <bastiaan.schaap@kvk.nl>
2015-11-04 13:40:30 +0100

    Conditionally set correct parameters

--- manifests/datasources/driver.pp
+++ manifests/datasources/driver.pp
@@ -3,11 +3,19 @@
 #
 define wildfly::datasources::driver($driver_name, $driver_module_name, $driver_class_name = undef, $driver_xa_datasource_class_name = undef, $target_profile = undef) {
 
-  $params = {
-    'driver-name' => $driver_name,
-    'driver-module-name' => $driver_module_name,
-    'driver-class-name' => $driver_class_name,
-    'driver-xa-datasource-class-name' => $driver_xa_datasource_class_name
+
+  if $driver_class_name {
+    $params = {
+      'driver-name' => $driver_name,
+      'driver-module-name' => $driver_module_name,
+      'driver-class-name' => $driver_class_name
+    }
+  } else {
+    $params = {
+      'driver-name' => $driver_name,
+      'driver-module-name' => $driver_module_name,
+      'driver-xa-datasource-class-name' => $driver_xa_datasource_class_name
+    }
   }
 
   wildfly::util::resource { "/subsystem=datasources/jdbc-driver=${driver_name}":
