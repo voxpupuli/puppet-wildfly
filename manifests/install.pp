2ebaf8603404630650840449c830499b17a500cb
Matt Peter <mpeter@redhat.com>
2016-01-08 23:30:29 -0500

    Make tarball installation directory configurable

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,22 +4,22 @@
 class wildfly::install  {
 
   $install_source = $wildfly::install_source
-
+  $install_cache_dir = $wildfly::install_cache_dir
   $install_file = inline_template('<%=File.basename(URI::parse(@install_source).path)%>')
 
   # Download Wildfly from jboss.org
   exec { "Download wildfly from ${install_source}":
-    command  => "wget -N -P /var/cache/wget ${install_source} --max-redirect=5",
+    command  => "wget -N -P ${install_cache_dir} ${install_source} --max-redirect=5",
     path     => ['/bin', '/usr/bin', '/sbin'],
     loglevel => 'notice',
-    creates  => "/var/cache/wget/${install_file}",
+    creates  => "${install_cache_dir}/${install_file}",
     unless   => "test -f ${wildfly::dirname}/jboss-modules.jar",
     timeout  => 500,
   }
   ~>
   # Gunzip+Untar wildfly.tar.gz if download was successful.
   exec { "untar ${install_file}":
-    command  => "tar --no-same-owner --no-same-permissions --strip-components=1 -C ${wildfly::dirname} -zxvf /var/cache/wget/${install_file}",
+    command  => "tar --no-same-owner --no-same-permissions --strip-components=1 -C ${wildfly::dirname} -zxvf ${install_cache_dir}/${install_file}",
     path     => ['/bin', '/usr/bin', '/sbin'],
     loglevel => 'notice',
     creates  => "${wildfly::dirname}/jboss-modules.jar",
