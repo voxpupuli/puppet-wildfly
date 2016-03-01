482aeae517efa23e145e7d663c800087b8d5be5c
Bastiaan Schaap <bastiaan.schaap@kvk.nl>
2016-03-01 12:33:03 +0100

    Add enable flag, and some SSL config options

--- manifests/undertow/https.pp
+++ manifests/undertow/https.pp
@@ -1,19 +1,23 @@
 #
 # Configures a connector
 #
-define wildfly::undertow::https($socket_binding = undef, $keystore_path = undef, $keystore_password = undef, $key_alias = undef, $key_password = undef, $target_profile = undef) {
+define wildfly::undertow::https($socket_binding = undef, $keystore_path = undef, $keystore_relative_to = undef, $keystore_password = undef, $key_alias = undef, $key_password = undef, $target_profile = undef, $enabled_protocols = undef, $enabled_cipher_suites = undef) {
 
   if versioncmp($wildfly::version,'9.0.0') >= 0 {
     # Wildfly 9 and higher -> we need to set enable parameter
     $listener_content = {
-      'socket-binding' => $socket_binding,
-      'security-realm' => 'TLSRealm',
-      'enable'         => true,
+      'socket-binding'        => $socket_binding,
+      'security-realm'        => 'TLSRealm',
+      'enabled-protocols'     => $enabled_protocols,
+      'enabled-cipher-suites' => $enabled_cipher_suites,
+      'enabled'               => true,
     }
   } else {
     $listener_content = {
-      'socket-binding' => $socket_binding,
-      'security-realm' => 'TLSRealm',
+      'socket-binding'        => $socket_binding,
+      'security-realm'        => 'TLSRealm',
+      'enabled-protocols'     => $enabled_protocols,
+      'enabled-cipher-suites' => $enabled_cipher_suites,
     }
   }
 
@@ -23,10 +27,11 @@ define wildfly::undertow::https($socket_binding = undef, $keystore_path = undef,
   ->
   wildfly::util::resource { '/core-service=management/security-realm=TLSRealm/server-identity=ssl':
     content => {
-      'keystore-path'     => $keystore_path,
-      'keystore-password' => $keystore_password,
-      'alias'             => $key_alias,
-      'key-password'      => $key_password
+      'keystore-path'        => $keystore_path,
+      'keystore-relative-to' => $keystore_relative_to,
+      'keystore-password'    => $keystore_password,
+      'alias'                => $key_alias,
+      'key-password'         => $key_password
     },
   }
   ->
