ff4b90b58b206199dfca2f4287d8ecfec1d8bff7
Alexey Yamschikov <mobilesfinks@gmail.com>
2016-08-05 14:48:15 +0200

    Fix issue #134

--- manifests/config/user.pp
+++ manifests/config/user.pp
@@ -9,6 +9,12 @@ define wildfly::config::user(
 
   $password_hash = inline_template('<%= require \'digest/md5\'; Digest::MD5.hexdigest("#{@username}:#{@realm}:#{@password}") %>')
 
+  file { "${wildfly::dirname}/${wildfly::mode}/configuration/${file_name}":
+      ensure  => file,
+      owner   => $wildfly::user,
+      group   => $wildfly::group,
+  }
+
   file_line { "${username}:${realm}":
     path  => "${wildfly::dirname}/${wildfly::mode}/configuration/${file_name}",
     line  => "${username}=${password_hash}",
