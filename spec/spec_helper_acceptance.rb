8478d44fcc3898ecf220b3fae0a269f3f4b55c97
Jairo Junior <junior.jairo1@gmail.com>
2016-11-20 20:59:13 -0300

    Update beaker helper to install requirements.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,11 +1,6 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
-# hosts.each do |host|
-#   # Install Puppet
-#   on host, install_puppet
-# end
-
 RSpec.configure do |c|
   project_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
@@ -14,9 +9,10 @@ RSpec.configure do |c|
   c.before :suite do
     puppet_module_install(:source => project_root, :module_name => 'wildfly')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib', '--force', '--version', '4.2.0'), :acceptable_exit_codes => [0, 1]
-      on host, puppet('module','install','puppetlabs-java'), { :acceptable_exit_codes => [0, 1] }
-      on host, puppet('module','install','puppet-archive'), { :acceptable_exit_codes => [0, 1] }
+      on host, puppet('module', 'install', 'puppetlabs-stdlib', '--force', '--version', '4.2.0')
+      on host, puppet('module','install','puppetlabs-java')
+      on host, puppet('module','install','puppet-archive')
+      on host, puppet('apply', '-e', %{"exec { 'gem install treetop net-http-digest_auth': path => '/opt/puppetlabs/puppet/bin/' }"})
     end
   end
 end
