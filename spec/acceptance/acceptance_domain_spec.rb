69580dc9391a86316b62f57f0811528b54b92e17
Jairo Junior <junior.jairo1@gmail.com>
2016-11-23 09:33:01 -0300

    Improve acceptance tests to better fit version X OS matrix model.

--- spec/acceptance/acceptance_domain_spec.rb
+++ spec/acceptance/acceptance_domain_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper_acceptance'
-require 'json'
 
-describe "Acceptance case five. Deployment on standalone mode with #{test_data['distribution']}:#{test_data['version']}" do 
-  context 'Initial install Wildfly, deployment and verification' do
+describe "Domain mode with #{test_data['distribution']}:#{test_data['version']}" do
+  context 'Initial install Wildfly and verification' do
     it 'applies the manifest without error' do
       pp = <<-EOS
           case $::osfamily {
@@ -31,11 +30,9 @@ describe "Acceptance case five. Deployment on standalone mode with #{test_data['
             version        => '#{test_data['version']}',
             install_source => '#{test_data['install_source']}',
             java_home      => $java_home,
-          } ->
-
-          wildfly::deployment { 'hawtio.war':
-            source => 'http://central.maven.org/maven2/io/hawt/hawtio-web/1.4.66/hawtio-web-1.4.66.war'
+            mode           => 'domain',
           }
+
       EOS
 
       # Run it twice and test for idempotency
@@ -49,29 +46,13 @@ describe "Acceptance case five. Deployment on standalone mode with #{test_data['
       expect(service('wildfly')).to be_running
     end
 
-    it 'runs on port 8080' do
-      expect(port(8080)).to be_listening
+    it 'runs on port 9990' do
+      expect(port(9990)).to be_listening
     end
 
-    it 'welcome page' do
-      shell('curl localhost:8080', acceptable_exit_codes: 0) do |r|
-        expect(r.stdout).to include 'Welcome'
-      end
-    end
-
-    it 'downloaded WAR file' do
-      shell('ls -la /tmp/hawtio-web-1.4.66.war', acceptable_exit_codes: 0) do |r|
-        expect(r.stdout).to include '/tmp/hawtio-web-1.4.66.war'
-      end
-    end
-
-    it 'deployed application' do
-      shell('/opt/wildfly/bin/jboss-cli.sh --connect "/deployment=hawtio.war:read-resource(recursive=true)"',
-            acceptable_exit_codes: 0) do |r|
-        expect(r.stdout).to include '"outcome" => "success"'
-      end
-      shell('curl localhost:8080/'.concat('hawtio/'), acceptable_exit_codes: 0) do |r|
-        expect(r.stdout).to include 'hawtio'
+    it 'protected management page' do
+      shell('curl localhost:9990/management', acceptable_exit_codes: 0) do |r|
+        expect(r.stdout).to include '401 - Unauthorized'
       end
     end
   end
---
+++ spec/acceptance/acceptance_standalone_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper_acceptance'
+
+describe "Standalone mode with #{test_data['distribution']}:#{test_data['version']}" do
+  context 'Initial install Wildfly and verification' do
+    it 'applies the manifest without error' do
+      # update augeas on debian
+      # echo 'deb     http://pkg.camptocamp.net/apt wheezy/stable sysadmin' | sudo tee -a /etc/apt/sources.list
+      # curl -s http://pkg.camptocamp.net/packages-c2c-key.gpg | sudo apt-key add -
+      # apt-get update
+      # sudo apt-get install augeas-tools=1.\* augeas-lenses=1.\* augeas-doc=1.\* libaugeas0=1.\*
+      # dpkg -l '*augeas*'
+
+      pp = <<-EOS
+          case $::osfamily {
+            'RedHat': {
+              java::oracle { 'jdk8' :
+                ensure  => 'present',
+                version => '8',
+                java_se => 'jre',
+                before  => Class['wildfly']
+              }
+
+              $java_home = '/usr/java/default'
+             }
+            'Debian': {
+              class { 'java':
+                before => Class['wildfly']
+              }
+
+              $java_home = "/usr/lib/jvm/java-7-openjdk-${::architecture}"
+           }
+          }
+
+          class { 'wildfly':
+            distribution   => '#{test_data['distribution']}',
+            version        => '#{test_data['version']}',
+            install_source => '#{test_data['install_source']}',
+            java_home      => $java_home,
+            init_system    => $init_system,
+          } 
+
+          wildfly::config::module { 'empty.module':
+            source       => '.'
+          }
+
+          wildfly::config::module { 'org.postgresql':
+            source       => 'http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
+            dependencies => ['javax.api', 'javax.transaction.api']
+          }
+          ->
+          wildfly::datasources::driver { 'Driver postgresql':
+            driver_name                     => 'postgresql',
+            driver_module_name              => 'org.postgresql',
+            driver_xa_datasource_class_name => 'org.postgresql.xa.PGXADataSource'
+          }
+          ->
+          wildfly::datasources::datasource { 'DemoDS':
+            config         => {
+              'driver-name'    => 'postgresql',
+              'connection-url' => 'jdbc:postgresql://localhost/postgres',
+              'jndi-name'      => 'java:jboss/datasources/DemoDS',
+              'user-name'      => 'postgres',
+              'password'       => 'postgres'
+            }
+          }
+          ->
+          wildfly::cli { ':reload':
+            onlyif => '(result == reload-required) of :read-attribute(name=server-state)',
+          }
+
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true, acceptable_exit_codes: [0, 2])
+      expect(apply_manifest(pp, catch_failures: true).exit_code).to be_zero
+      shell('sleep 15')
+    end
+
+    it 'service wildfly' do
+      expect(service('wildfly')).to be_enabled
+      expect(service('wildfly')).to be_running
+    end
+
+    it 'runs on port 8080' do
+      expect(port(8080)).to be_listening
+    end
+
+    it 'welcome page' do
+      shell('curl localhost:8080', acceptable_exit_codes: 0) do |r|
+        expect(r.stdout).to include 'Welcome'
+      end
+    end
+
+    it 'contains postgresql module' do
+      shell('ls -la /opt/wildfly/modules/system/layers/base/org/postgresql/main', acceptable_exit_codes: 0) do |r|
+        expect(r.stdout).to include 'postgresql-9.3-1103-jdbc4.jar'
+        expect(r.stdout).to include 'module.xml'
+      end
+    end
+
+    it 'contains empty module' do
+      shell('ls -la /opt/wildfly/modules/system/layers/base/empty/module/main', acceptable_exit_codes: 0) do |r|
+        expect(r.stdout).to include 'module.xml'
+      end
+      shell('cat /opt/wildfly/modules/system/layers/base/empty/module/main/module.xml', acceptable_exit_codes: 0) do |r|
+        expect(r.stdout).to include '<resource-root path="."/>'
+      end
+    end
+  end
+end
