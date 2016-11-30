2459aed59c7dcca9baa445fe08275c3446d7bccb
Jairo Junior <junior.jairo1@gmail.com>
2016-11-30 12:26:42 -0300

    Make class compatible with old Puppet users.

--- manifests/service/systemd.pp
+++ manifests/service/systemd.pp
@@ -6,13 +6,13 @@ class wildfly::service::systemd {
 
   if $wildfly::service::systemd_native {
     file { "${wildfly::dirname}/bin/launch.sh" :
-      ensure  => present,
-      mode    => '0755',
-      owner   => $wildfly::user,
-      group   => $wildfly::group,
+      ensure => present,
+      mode   => '0755',
+      owner  => $wildfly::user,
+      group  => $wildfly::group,
       # need to manage this until it's fixed in the upstream
-      content => file('wildfly/launch.sh'),
-      before  => File["/etc/systemd/system/${wildfly::service::service_name}.service"],
+      source => 'puppet:///modules/wildfly/launch.sh',
+      before => File["/etc/systemd/system/${wildfly::service::service_name}.service"],
     }
   } else {
     # Use init.d scripts for systemd
