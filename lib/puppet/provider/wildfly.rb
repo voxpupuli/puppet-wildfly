c2b87b4f6cc132b25df00cba71384ad5c35766a1
Jairo Junior <junior.jairo1@gmail.com>
2016-12-30 07:04:55 -0300

    Minor issue fix.

--- lib/puppet/provider/wildfly.rb
+++ lib/puppet/provider/wildfly.rb
@@ -11,7 +11,7 @@ class Puppet::Provider::Wildfly < Puppet::Provider
   def cli
     require 'puppet_x/wildfly/api_client'
     require 'puppet_x/wildfly/operation_request'
-    timeout = @resource.parameters.include?(:timeout)? resource[:timeout] : 60
+    timeout = @resource.parameters.include?(:timeout) ? resource[:timeout] : 60
 
     api_client = PuppetX::Wildfly::APIClient.new(@resource[:host], @resource[:port], @resource[:username], @resource[:password], timeout)
     PuppetX::Wildfly::OperationRequest.new(api_client)
