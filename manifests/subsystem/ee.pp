6de8664dc5838950d268e469e4b403b352ee6f0c
mtdjr <mtdjr@users.noreply.github.com>
2017-09-15 13:38:14 -0400

    Added ability to set deployments and default bindings attributes
    Needed the ability to adjust the EE settings. I used structs as the attribute names are fixed, but they are all optional. Only those that are provided/modified will be changed by Puppet. If you only care about Global Modules, then you need only include that in your hash. 
    
    There may be a better way to solve this; I'm open suggestions.

--- manifests/subsystem/ee.pp
+++ manifests/subsystem/ee.pp
@@ -1,14 +1,32 @@
 #
 # Configures EE Subsystem
 #
+# @ param deployments hash of the Global EE Deployments Attributes - 
+# @ param default_bindings hash of the Global EE Default Bindings Attributes
+#
 
 define wildfly::subsystem::ee(
-
-#params
-
+  Struct[{annotation-property-replacement => Optional[Boolean],
+          ear-subdeployments-isolated => Optional[Boolean],
+          jboss-descriptor-property-replacement => Optional[Boolean],
+          spec-descriptor-property-replacement => Optional[Boolean],
+          global-modules => Optional[Array]}] $deployments                 = undef,
+  Struct[{context-service => Optional[String],
+          datasource => Optional[String],
+          jms-connection-factory => Optional[String],
+          managed-executor-service => Optional[String],
+          managed-scheduled-executor-service => Optional[String],
+          managed-thread-factory => Optional[String]}] $default_bindings   = undef,
 ){
 
-  wildfly::resource{'':
-  
+  if $deployments {
+    wildfly::resource{'/subsystem=ee':
+      content => $deployments,
+    }
+  }
+  if $default_bindings {
+    wildfly::resource {'/subsystem=ee/service=default-bindings':
+      content => $default_bindings,
+    }
   }
 }
