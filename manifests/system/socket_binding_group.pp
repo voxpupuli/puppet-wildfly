f78ca5773a287b4af9192b4197ea3132b07c6140
mtdjr <mtdjr@users.noreply.github.com>
2017-09-19 15:24:58 -0400

    Create socket_binding_group.pp

---
+++ manifests/system/socket_binding_group.pp
@@ -0,0 +1,42 @@
+#
+# Configures Socket Binding Groups
+#   Uses $title as the Socket Binding Group name. You may have only one group at a time. Jboss default name is 'standard-sockets'
+# @param default_interface Name of an interface that should be used as the interface for any sockets that do not explicitly declare one. Defaults are 'management', 'public', 'unsecure'.
+# @param port_offset Increment to apply to the base port values defined in the socket bindings to derive the runtime values to use on this server.
+# @param socket-bindings Hash of hashes with your socket bindings in the form { 'socket_name' => { 'port' => 8080, ... } }
+
+define wildfly::system::socket_binding_group (
+  $default_interface                          = undef,
+  $port_offset                                = undef,
+  Hash $socket_bindings                       = undef,
+){
+
+  wildfly::resource { "/socket-binding-group=${title}":
+    content => {
+      'default-interface' => $default_interface,
+      'port-offset' => { 'EXPRESSION_VALUE' => $port_offset }
+    },
+  }
+  
+  $socket_bindings.each | $socket, $hash | {
+    if $hash['fixed-port'] and !$hash['fixed-port'].is_a(Integer) {
+      fail('fixed-port must be of type Integer')
+    }
+    if $hash['multicast-port'] and !$hash['multicast-port'].is_a(Integer) {
+      fail('multicast-port must be of type Integer')
+    }
+    if $hash['port'] and !$hash['port'].is_a(Integer) {
+      fail('port must be of type Integer')
+    }
+    wildfly::resource { "/socket-binding-group=${title}/socket-binding=${socket}":
+      content => {
+        'client-mappings' => $hash['client-mappings'],
+        'fixed-port' => $hash['fixed-port'],
+        'interface' => $hash['interface'],
+        'multicast-address' => $hash['multicast-address'],
+        'multicast-port' => $hash['multicast-port'],
+        'port' => $hash['port']
+      }
+    }
+  }
+}
