c9f708ea33bf5ec90e836dc8515d8cbe011214e0
mtdjr <mtdjr@users.noreply.github.com>
2017-09-20 13:11:58 -0400

    added target_profile param

--- manifests/logging/pattern_formatter.pp
+++ manifests/logging/pattern_formatter.pp
@@ -3,13 +3,15 @@
 #
 
 define wildfly::logging::pattern_formatter (
-  $color_map = undef,
-  $pattern   = undef,
+  $color_map      = undef,
+  $pattern        = undef,
+  $target_profile = undef,
 ){
   wildfly::resource { "/subsystem=logging/pattern-formatter=${title}":
     content => {
       'color-map' => $color_map,
       'pattern'   => $pattern
-    }
+    },
+    $target_profile = undef,
   }
 }
