57ca35c97d7d333aee0a1434d29cea99b3cba286
mtdjr <mtdjr@users.noreply.github.com>
2017-09-20 14:11:43 -0400

    remove target profile

--- manifests/logging/pattern_formatter.pp
+++ manifests/logging/pattern_formatter.pp
@@ -5,13 +5,11 @@
 define wildfly::logging::pattern_formatter (
   $color_map      = undef,
   $pattern        = undef,
-  $target_profile = undef,
 ){
   wildfly::resource { "/subsystem=logging/pattern-formatter=${title}":
     content => {
       'color-map' => $color_map,
       'pattern'   => $pattern
     },
-    target_profile => undef,
   }
 }
