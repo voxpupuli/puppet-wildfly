fe82f252146320b93b60134674e62044594e9a90
mtdjr <mtdjr@users.noreply.github.com>
2017-09-20 12:44:23 -0400

    Added role mapping creation option

--- manifests/security/user_role_mapping.pp
+++ manifests/security/user_role_mapping.pp
@@ -1,8 +1,18 @@
 #
-define wildfly::security::user_role_mapping($role, $username = $title, $realm = undef) {
+define wildfly::security::user_role_mapping (
+  $role,
+  $username = $title,
+  $realm = undef,
+  $create_role = false,
+){
 
   $_username = upcase($username)
 
+  if $create_role {
+    wildfly::resource { "/core-service=management/access=authorization/role-mapping=${role}":
+      before => Wildfly::Resource["/core-service=management/access=authorization/role-mapping=${role}/include=user-${_username}"],
+    }
+  }
   wildfly::resource { "/core-service=management/access=authorization/role-mapping=${role}/include=user-${_username}":
     content => {
       'name'  => $username,
@@ -10,5 +20,4 @@ define wildfly::security::user_role_mapping($role, $username = $title, $realm = 
       'type'  => 'USER',
     },
   }
-
 }
