d0c22a5e7286a4438abdb553096007eb6f3d5c69
mtdjr <mtdjr@users.noreply.github.com>
2017-09-20 13:05:05 -0400

    Create logging/pattern-formatter

---
+++ manifests/logging/pattern_formatter.pp
@@ -0,0 +1,15 @@
+#
+#
+#
+
+define wildfly::logging::pattern_formatter (
+  $color_map = undef,
+  $pattern   = undef,
+){
+  wildfly::resource { "/subsystem=logging/pattern-formatter=${title}":
+    content => {
+      'color-map' => $color_map,
+      'pattern'   => $pattern
+    }
+  }
+}
