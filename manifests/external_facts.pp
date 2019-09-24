7aaa94e9c54c5bdb915a83c91f41fe8b98a68927
Thiago David <thiago.david@dataprev.gov.br>
2019-09-24 10:08:50 -0300

    Create external fact wildfly_is_running
    This fact will use pgrep with `$wildfly::dirname` to get a Process ID, then return "1" if any process exists. This can be a naive implementation and a more robust approach could be better suited

--- manifests/external_facts.pp
+++ manifests/external_facts.pp
@@ -5,4 +5,9 @@ class wildfly::external_facts() {
     content => epp('wildfly/facts.yaml'),
   }
 
+  file { '/opt/puppetlabs/facter/facts.d/wildfly_is_running.sh':
+    ensure  => file,
+    mode    => '1755',
+    content => epp('wildfly/wildfly_is_running.sh'),
+  }
 }
