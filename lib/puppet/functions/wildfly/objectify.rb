2b22d49c861305cbc89654e8038ebb2d6d1fe420
Robert Waffen <rwaffen@gmail.com>
2023-10-13 15:45:39 +0200

    replace puppet function with ruby function

---
+++ lib/puppet/functions/wildfly/objectify.rb
@@ -0,0 +1,25 @@
+# Create a Puppet function named 'wildfly::objectify'
+# This function takes an array as an argument and converts it into a hash.
+Puppet::Functions.create_function(:'wildfly::objectify') do
+  dispatch :objectify do
+    param 'Array[Variant[String, Hash[String, Any]]]', :input_list
+  end
+
+  def objectify(input_list)
+    result_hash = {}
+
+    input_list.each do |item|
+      if item.is_a?(String)
+        # If the item is a string, add it to the hash with a default key
+        result_hash[item] = {}
+      elsif item.is_a?(Hash)
+        # If the item is a hash, merge it into the result_hash
+        result_hash.merge!(item)
+      else
+        raise Puppet::ParseError, "Invalid data type in the list"
+      end
+    end
+
+    result_hash
+  end
+end
